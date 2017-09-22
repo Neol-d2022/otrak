f0aea0735f0776f8636fa148dc41711a576d91a7
neol-d2022 <neol_d2022@outlook.com>
2017-09-22 10:02:35 +0800

    Module Manager (login func prototype)

---
+++ source/manager.js
@@ -0,0 +1,24 @@
+var moduleManager = module.exports = {}
+
+var userList = {
+    user: { id: 'user', pw: 'user' },
+    pet: { id: 'pet', pw: 'pet' }
+}
+
+moduleManager.login = function (id, pw) {
+    var user = userList[id];
+
+    if (user.pw === pw)
+        return user
+    //else
+    //    return undefined
+}
+
+var userTests = [
+    moduleManager.login('user', 'user'),
+    moduleManager.login('pet', 'blackie'),
+]
+
+for (var i = 0; i < userTests.length; i += 1) {
+    console.log('user[' + i + '] = ' + JSON.stringify(userTests[i]));
+}
