713a37147a0699db66f5fe7fa7e54f81dd080caa
neol-d2022 <neol_d2022@outlook.com>
2017-09-22 11:05:04 +0800

    Added test script for login in manager.js

---
+++ source/loginTest.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+describe('login', function () {
+    var moduleManager = require('./manager.js')
+    describe('login-user-user', function () {
+        it('should return a user object', function () {
+            assert.equal('user', moduleManager.login('user', 'user').id);
+        });
+    });
+    describe('login-pet-blackie', function () {
+        it('should fail(return undefined)', function () {
+            assert.equal('undefined', typeof (moduleManager.login('pet', 'blackie')));
+        });
+    });
+});
